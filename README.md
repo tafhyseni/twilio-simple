74eb7e049064d4500e5b08ec4ab6515318cd88df
Tafinjo <tafhyseni@gmail.com>
2019-12-05 13:15:44 +0100

    Initialization

---
+++ README.md
@@ -0,0 +1,53 @@
+# Very short description of the package
+
+[![Latest Version on Packagist](https://img.shields.io/packagist/v/tafhyseni/twilio-simple.svg?style=flat-square)](https://packagist.org/packages/tafhyseni/twilio-simple)
+[![Build Status](https://img.shields.io/travis/tafhyseni/twilio-simple/master.svg?style=flat-square)](https://travis-ci.org/tafhyseni/twilio-simple)
+[![Quality Score](https://img.shields.io/scrutinizer/g/tafhyseni/twilio-simple.svg?style=flat-square)](https://scrutinizer-ci.com/g/tafhyseni/twilio-simple)
+[![Total Downloads](https://img.shields.io/packagist/dt/tafhyseni/twilio-simple.svg?style=flat-square)](https://packagist.org/packages/tafhyseni/twilio-simple)
+
+This is where your description should go. Try and limit it to a paragraph or two, and maybe throw in a mention of what PSRs you support to avoid any confusion with users and contributors.
+
+## Installation
+
+You can install the package via composer:
+
+```bash
+composer require tafhyseni/twilio-simple
+```
+
+## Usage
+
+``` php
+// Usage description here
+```
+
+### Testing
+
+``` bash
+composer test
+```
+
+### Changelog
+
+Please see [CHANGELOG](CHANGELOG.md) for more information what has changed recently.
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+### Security
+
+If you discover any security related issues, please email tafhyseni@gmail.com instead of using the issue tracker.
+
+## Credits
+
+- [Mustafe Hyseni](https://github.com/tafhyseni)
+- [All Contributors](../../contributors)
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
+
+## PHP Package Boilerplate
+
+This package was generated using the [PHP Package Boilerplate](https://laravelpackageboilerplate.com).
---
+++ src/RequestTwilio.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tafhyseni\TwilioSimple;
+
+use GuzzleHttp\Client;
+use Exception;
+
+class RequestTwilio
+{
+
+	/**
+	 * @var string The Twilio SID
+	 */
+	protected $SID;
+
+	/**
+	 * @var string The Twilio API Token
+	 */
+	protected $TOKEN;
+
+	/**
+	 * @var string A Valid Twilio phone number
+	 */
+	protected $phone_number;
+
+	/**
+	 * @var int SMS LIMIT (before split). It's already a defined constant by Twilio!
+	 */
+	const LIMIT = 160;
+
+	/**
+	 * SMS Message
+	 * @var string
+	 */
+	public $message;
+
+	protected $twilio;
+
+    public function __construct($sid = NULL, $token = NULL, $phone = NULL)
+    {
+    	if(!isset($sid) || !isset($token))
+    	{
+    		throw new Exception('Configuration failure. Make sure you set SID and Token from Twilio Console!');
+    	}
+        $this->SID = $sid;
+        $this->TOKEN = $token;
+        $this->PHONE = $phone;
+    }
+
+    /**
+     * SMS Message setter
+     * @param string $message SMS Message
+     */
+    public function setSMS($message)
+    {
+		$this->message = $message;    	
+    }
+
+    /**
+     * SMS Message getter
+     * @return string SMS Message
+     */
+    public function getSMS()
+    {
+    	return $this->message;
+    }
+
+    /**
+     * Preview the SMS message body
+     * @return string message preview
+     */
+    public function preview()
+    {
+    	//
+    }
+
+    /**
+     * Returns the number of SMS's needed for the SMS message
+     * @return int
+     */
+    public function countSMS()
+    {
+    	return count(str_split($this->message, self::LIMIT));
+    }
+
+    /**
+     * Sends the SMS request to Twilio API
+     * @return array Returns array will response code and message
+     */
+    public function sendSMS($phone_number)
+    {
+    	// Send SMS
+    }
+
+    /**
+     * Connection will be done only on several functions
+     * @return bool
+     */
+    public function connectToTwilio()
+    {
+    	$client = new \GuzzleHttp\Client();
+		// $this->twilio = new Client($this->sid, $this->token);
+		// return $this->twilio;
+    }
+}
